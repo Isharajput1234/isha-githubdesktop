12eebc31bfb6ba95fee73545b5b34ae50c918d1d
Isharajput1234 <158489407+Isharajput1234@users.noreply.github.com>
2024-02-15 23:33:04 +0530

    Update file1.c
    pallindrome

--- file1.c
+++ file1.c
@@ -8,7 +8,7 @@ int main()
     printf("input a string");
     fget(s1,20,stdin);
     l=strlen(s1);
-    for(i=0,j=l-1;i<j;i++,j++)
+    for(i=0,j=l-1;i<j;i++,j--)
     {
         if(s1[i]!=s1[j])
         {
